71d4626dc8c3eef2df515d59cc8f5e31fb0a07a4
name <cybersteer@naver.com>
2022-03-15 19:43:02 +0900

    week02

---
+++ week02/app/src/main/java/com/example/week02/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.week02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnOk;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "장성익 010-5296-7638", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
