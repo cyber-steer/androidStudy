ed40a7a9502240b90a1960147fddfe666969795f
name <cybersteer@naver.com>
2022-03-16 00:06:05 +0900

    cal UI

--- week02_cal/app/src/main/java/com/example/week02_cal/MainActivity.java
+++ week02_cal/app/src/main/java/com/example/week02_cal/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.week02_cal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text, txtResult;
+    Integer result;
+    Integer[] numBtnIds = {R.id.btn0,R.id.btn1,R.id.btn2,
+            R.id.btn3,R.id.btn4,R.id.btn5,R.id.btn6,
+            R.id.btn7,R.id.btn8,R.id.btn9};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
